3e4200c2cd8f250e0022d6b27f3ffe09521388cc
aroggek <56154090+aroggek@users.noreply.github.com>
2022-05-15 00:28:16 +0300

    Update script_fot_pull_request_on_other+nodes.py

--- script_fot_pull_request_on_other+nodes.py
+++ script_fot_pull_request_on_other+nodes.py
@@ -9,7 +9,7 @@ ip_list=ip.replace(' ','').split(',')
 #hosts = open('/etc/hosts','r')
 hostname = socket.gethostname()
 my_host=(socket.gethostbyname(hostname))
-print(ip_list)
+#print(ip_list)
 for i in ip_list:
     if i != my_host:
        b = os.system('<path to script>/post-receive.sh' + ' ' + i )
