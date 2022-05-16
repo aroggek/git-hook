aff3275b61ec36d810884709b53d2f4284e8d8db
aroggek <56154090+aroggek@users.noreply.github.com>
2022-05-16 11:20:08 +0300

    Update script_fot_pull_request_on_other+nodes.py

--- script_fot_pull_request_on_other+nodes.py
+++ script_fot_pull_request_on_other+nodes.py
@@ -1,10 +1,11 @@
-#! usr/bin/python3
+#!/usr/bin/python3
 
-import os
 import socket
+import subprocess
 
-user='your username'
-ip='ypur ip list or you can read the file with ip's'
+
+user='<username>'
+ip='<some ip>, <some ip>, <some ip>'
 ip_list=ip.replace(' ','').split(',')
 #hosts = open('/etc/hosts','r')
 hostname = socket.gethostname()
@@ -12,4 +13,10 @@ my_host=(socket.gethostbyname(hostname))
 #print(ip_list)
 for i in ip_list:
     if i != my_host:
-       b = os.system('<path to script>/post-receive.sh' + ' ' + i )
+        sshProcess = subprocess.Popen(['ssh', i], stdin=subprocess.PIPE, 
+                               stdout = subprocess.PIPE,
+                               universal_newlines=True,
+                               bufsize=0)
+        sshProcess.stdin.write("cd $HOME/git/projects/\n")
+        sshProcess.stdin.write("git pull origin master\n")
+        sshProcess.stdin.close()
